9a66dea9cd2a0a85b17383ef009ce1b88b890177
Kaeyn <94393201+Kaeyn@users.noreply.github.com>
2024-05-08 17:33:48 +0700

    HamperDetail-InConfig002-Update: Thêm data cho dropdown, các component: list, company

--- src/app/in-app/in-config/pages/config002-hamper-detail/config002-hamper-detail.component.ts
+++ src/app/in-app/in-config/pages/config002-hamper-detail/config002-hamper-detail.component.ts
@@ -1,6 +1,9 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { BreadCrumbItem } from '@progress/kendo-angular-navigation';
 import { plusIcon, undoIcon, redoIcon, checkOutlineIcon, minusOutlineIcon, trashIcon } from '@progress/kendo-svg-icons';
+import { companyVietHaTri, company3PS, companyMotThanhVien } from '../config004-hamper-detail/data-test';
+import { DataUnitProduct } from '../config004-hamper-detail/dataUnitProduct';
+import { dataMadeHameper } from '../config004-hamper-detail/dataMadeOfHamper';
 @Component({
   encapsulation: ViewEncapsulation.None,
   selector: 'app-config002-hamper-detail',
@@ -23,6 +26,11 @@ export class Config002HamperDetailComponent {
   ];
 
   buttonIcons = {plusIcon, undoIcon, redoIcon, checkOutlineIcon, minusOutlineIcon, trashIcon, chevronDownIcon : "k-i-arrow-chevron-down"}
+
+  companyData = {companyVietHaTri,company3PS,companyMotThanhVien};
+  unitData = {DataUnitProduct};
+  originData = {dataMadeHameper}
+
 
   openDrawer(){
     this.isDrawerShow = true;
