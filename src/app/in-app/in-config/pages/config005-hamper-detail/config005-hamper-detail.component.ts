42cf88ca7e34a461b7f7e5d09a6b98cd6ed2326a
TanHieu251 <buitanhieu000@gmail.com>
2024-05-10 09:50:47 +0700

    hamber -  in-app\in-config\pages\config005-hamper-detail - update : chỉnh sửa lại form

--- src/app/in-app/in-config/pages/config005-hamper-detail/config005-hamper-detail.component.ts
+++ src/app/in-app/in-config/pages/config005-hamper-detail/config005-hamper-detail.component.ts
@@ -1,22 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { NotifiService } from '../shared/services/notifi.service';
 import { BreadCrumbItem } from '@progress/kendo-angular-navigation';
 import { checkOutlineIcon } from '@progress/kendo-svg-icons';
 import { FormControl, FormGroup } from '@angular/forms';
 import { dataMadeHameper } from '../config004-hamper-detail/dataMadeOfHamper';
 import { DataUnitProduct } from '../config004-hamper-detail/dataUnitProduct';
+import { DTOHamper } from '../shared/dtos/DTOHamper.dto';
 @Component({
   selector: 'app-config005-hamper-detail',
   templateUrl: './config005-hamper-detail.component.html',
   styleUrls: ['./config005-hamper-detail.component.scss'],
 })
-export class Config005HamperDetailComponent {
-  hamber = new FormGroup({
+export class Config005HamperDetailComponent implements OnInit, OnChanges {
+  hamberForm = new FormGroup({
     barcode: new FormControl({ value: '', disabled: true }),
-    vietnamese: new FormControl(''),
+    vietnamese: new FormControl(),
     english: new FormControl(''),
     japanese: new FormControl(''),
-    origin: new FormControl(''),
+    origin: new FormControl({ value: '', disabled: true }),
     materialVietnamese: new FormControl(''),
     materialEnglish: new FormControl(''),
     materialJapanese: new FormControl(''),
@@ -50,27 +51,14 @@ export class Config005HamperDetailComponent {
     pallet: new FormControl(''),
     others: new FormControl(''),
   });
-  defultOrigin: { made: string; id: number } = {
-    id: 0,
-    made: 'Việt Nam',
-  };
+  // defultOrigin: { made: string; id: number } = {
+  //   id: 0,
+  //   made: 'Việt Nam',
+  // };
   defaultUnit: { made: string; id: number } = {
     id: 2,
     made: 'Gam',
   };
-  public origins = dataMadeHameper;
-  public unit = DataUnitProduct;
-  public showDrawer: boolean = false;
-
-  constructor(private notification: NotifiService) {}
-
-  icons = { checkOutlineIcon };
-
-  // Confirm delete action
-  confirmDelete() {
-    this.notification.message('Thêm thành công', 'success');
-  }
-
   itemBreadCrumb: BreadCrumbItem[] = [
     {
       text: 'QUẢN LÝ SẢN PHẨM',
@@ -82,8 +70,22 @@ export class Config005HamperDetailComponent {
       text: 'CHI TIẾT HAMPER',
     },
   ];
+  icons = { checkOutlineIcon };
+  confirmDelete() {
+    this.notification.message('Thêm thành công', 'success');
+  }
 
+  public origins = dataMadeHameper;
+  public unit = DataUnitProduct;
+  public showDrawer: boolean = false;
+  hamber: DTOHamper[] = [];
+
+  constructor(private notification: NotifiService) {}
   toggleDrawer() {
     this.showDrawer = !this.showDrawer;
   }
+  ngOnChanges(changes: SimpleChanges): void {}
+  ngOnInit(): void {}
+
+  // Create hamper object
 }
