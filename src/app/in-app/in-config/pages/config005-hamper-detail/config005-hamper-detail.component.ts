a8cdfcb6afc76ddcd049a7461eec93305f42f266
TanHieu251 <buitanhieu000@gmail.com>
2024-05-10 13:41:58 +0700

    hamber -  in-app\in-config\pages\config005-hamper-detail - add : tạo mảng hamper khi blur input

--- src/app/in-app/in-config/pages/config005-hamper-detail/config005-hamper-detail.component.ts
+++ src/app/in-app/in-config/pages/config005-hamper-detail/config005-hamper-detail.component.ts
@@ -6,55 +6,56 @@ import { FormControl, FormGroup } from '@angular/forms';
 import { dataMadeHameper } from '../config004-hamper-detail/dataMadeOfHamper';
 import { DataUnitProduct } from '../config004-hamper-detail/dataUnitProduct';
 import { DTOHamper } from '../shared/dtos/DTOHamper.dto';
+import { DTOHamperTest } from '../shared/dtos/DTOHamperTest.dto';
 @Component({
   selector: 'app-config005-hamper-detail',
   templateUrl: './config005-hamper-detail.component.html',
   styleUrls: ['./config005-hamper-detail.component.scss'],
 })
 export class Config005HamperDetailComponent implements OnInit, OnChanges {
-  hamberForm = new FormGroup({
-    barcode: new FormControl({ value: '', disabled: true }),
-    vietnamese: new FormControl(),
-    english: new FormControl(''),
-    japanese: new FormControl(''),
-    origin: new FormControl({ value: '', disabled: true }),
-    materialVietnamese: new FormControl(''),
-    materialEnglish: new FormControl(''),
-    materialJapanese: new FormControl(''),
-    baseUnit: new FormControl(''),
-    sizeProduct: new FormGroup({
-      width: new FormControl(''),
-      height: new FormControl(''),
-      length: new FormControl(''),
-      weight: new FormControl(''),
-    }),
-    sizeInner: new FormGroup({
-      width: new FormControl(''),
-      height: new FormControl(''),
-      length: new FormControl(''),
-      weight: new FormControl(''),
-    }),
-    sizeCartoon: new FormGroup({
-      width: new FormControl(''),
-      height: new FormControl(''),
-      length: new FormControl(''),
-      weight: new FormControl(''),
-    }),
-    sizePallet: new FormGroup({
-      width: new FormControl(''),
-      height: new FormControl(''),
-      length: new FormControl(''),
-      weight: new FormControl(''),
-    }),
-    inner: new FormControl(''),
-    cartoon: new FormControl(''),
-    pallet: new FormControl(''),
-    others: new FormControl(''),
-  });
-  // defultOrigin: { made: string; id: number } = {
-  //   id: 0,
-  //   made: 'Việt Nam',
-  // };
+  hamberForm = new FormGroup(
+    {
+      barcode: new FormControl({ value: '', disabled: true }),
+      vietnamese: new FormControl(),
+      english: new FormControl(''),
+      japanese: new FormControl(''),
+      origin: new FormControl({ value: '', disabled: true }),
+      materialVietnamese: new FormControl(''),
+      materialEnglish: new FormControl(''),
+      materialJapanese: new FormControl(''),
+      baseUnit: new FormControl(''),
+      sizeProduct: new FormGroup({
+        width: new FormControl(''),
+        height: new FormControl(''),
+        length: new FormControl(''),
+        weight: new FormControl(''),
+      }),
+      sizeInner: new FormGroup({
+        width: new FormControl(''),
+        height: new FormControl(''),
+        length: new FormControl(''),
+        weight: new FormControl(''),
+      }),
+      sizeCartoon: new FormGroup({
+        width: new FormControl(''),
+        height: new FormControl(''),
+        length: new FormControl(''),
+        weight: new FormControl(''),
+      }),
+      sizePallet: new FormGroup({
+        width: new FormControl(''),
+        height: new FormControl(''),
+        length: new FormControl(''),
+        weight: new FormControl(''),
+      }),
+      inner: new FormControl(''),
+      cartoon: new FormControl(''),
+      pallet: new FormControl(''),
+      others: new FormControl(''),
+    },
+    { updateOn: 'blur' }
+  );
+
   defaultUnit: { made: string; id: number } = {
     id: 2,
     made: 'Gam',
@@ -78,14 +79,107 @@ export class Config005HamperDetailComponent implements OnInit, OnChanges {
   public origins = dataMadeHameper;
   public unit = DataUnitProduct;
   public showDrawer: boolean = false;
-  hamber: DTOHamper[] = [];
+  // hamber: DTOHamper[] = [];
+  hamperArray: DTOHamperTest[] = [];
 
   constructor(private notification: NotifiService) {}
   toggleDrawer() {
     this.showDrawer = !this.showDrawer;
   }
-  ngOnChanges(changes: SimpleChanges): void {}
-  ngOnInit(): void {}
+  ngOnChanges(changes: SimpleChanges): void {
+    this.onVietnameseBlur();
+  }
+  ngOnInit(): void {
+    this.createHamper();
+    this.updateValues();
+  }
 
-  // Create hamper object
+  createHamper(): void {
+    const hamper: DTOHamperTest = {
+      State: 'Đang soạn thảo',
+      CompanyList: [],
+      TotalPrice: 0,
+      Currency: '',
+      OtherInfo: '',
+      ProductList: [],
+      InfoHamber: {
+        barcode: null,
+        vietnameseName: null,
+        englishName: null,
+        japaneseName: null,
+        origin: null,
+        vietnameseMaterial: null,
+        englishMaterial: null,
+        japaneseMaterial: null,
+        baseUnit: null,
+        productSize: {
+          long: null,
+          width: null,
+          height: null,
+          weight: null,
+        },
+        innerSize: {
+          long: null,
+          width: null,
+          height: null,
+          weight: null,
+        },
+        cartonSize: {
+          long: null,
+          width: null,
+          height: null,
+          weight: null,
+        },
+        palletSize: {
+          long: null,
+          width: null,
+          height: null,
+          weight: null,
+        },
+        innerBaseUnit: null,
+        cartonBaseUnit: null,
+        palletInnerBaseUnit: null,
+      },
+    };
+
+    this.hamperArray.push(hamper);
+  }
+
+  onVietnameseBlur() {
+    const name = this.hamberForm.get('vietnamese').value;
+    if (name.trim().length > 0) {
+      if (this.hamperArray[0].InfoHamber.vietnameseName) {
+        this.hamperArray[0].InfoHamber.vietnameseName = name;
+        this.notification.message(
+          'Cập nhật Vietnamese name thành công',
+          'success'
+        );
+      } else {
+        this.hamperArray[0].InfoHamber.vietnameseName = name;
+        this.notification.message('Thêm Vietnamese name thành công', 'success');
+      }
+      console.log('hamperArray:', this.hamperArray); // Log the updated hamperArray
+    } else {
+      this.notification.message('Tên Vietnamese không được để trống', 'error');
+      return;
+    }
+  }
+
+  updateValues() {
+    this.hamberForm.valueChanges.subscribe((value) => {
+      const infoHamber: any = { ...value }; // Ép kiểu value sang any để tránh lỗi
+      this.hamperArray[0].InfoHamber = infoHamber;
+      console.log('hamperArray:', this.hamperArray);
+    });
+  }
+  data = [
+    {
+      Barcode: '0002',
+      Name: 'daudhahdoahdahdaljdaldhaldhaldaljbckhabvaihgcaousdh adhuahdkadakdadhadaldjaljdlajdlajdaldja',
+      Brand: 'Brand01',
+      Origin: 'Origin01',
+      Price: 15,
+      Quantity: 3,
+    },
+  ];
 }
