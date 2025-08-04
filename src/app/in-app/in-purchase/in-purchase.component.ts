f311d4286cf693d06f0fcb1f41cb729c7a665be3
Quang.Tran <tnq1130@gmail.com>
2025-08-04 10:26:26 +0700

    in-purchase-html/css/ts/routing/module: thêm module in-purchase, cấu hình routing cho in-purchase

---
+++ src/app/in-app/in-purchase/in-purchase.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-in-purchase',
+  templateUrl: './in-purchase.component.html',
+  styleUrls: ['./in-purchase.component.scss']
+})
+export class InPurchaseComponent {
+
+}
---
+++ src/app/in-app/in-purchase/in-purchase.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { InPurchaseRoutingModule } from './in-purchase-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    InPurchaseRoutingModule
+  ]
+})
+export class InPurchaseModule { }
