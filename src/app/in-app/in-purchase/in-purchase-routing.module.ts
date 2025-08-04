fc07e9f06f48ddf70bd27299ce95074dbb9501be
hale0904 <leduyha2809@gmail.com>
2025-08-04 14:25:41 +0700

    Purchase-Routing-Add: Thêm đường dẫn từ page Purchase Quote qua page Purchase Detail

--- src/app/in-app/in-purchase/in-purchase-routing.module.ts
+++ src/app/in-app/in-purchase/in-purchase-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PurchaseQuoteP001Component } from './pages/purchase-quote-p001/purchase-quote-p001.component';
+import { PurchaseDetailP001Component } from './pages/purchase-detail-p001/purchase-detail-p001.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PurchaseQuoteP001Component
+  },
+  {
+    path: 'purchase-detail-p001',
+    component: PurchaseDetailP001Component
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
