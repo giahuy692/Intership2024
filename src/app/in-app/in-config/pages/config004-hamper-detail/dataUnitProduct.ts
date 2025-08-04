dd953a26c33624697cb4f29c38bc684a47c9faff
Quang.Tran <tnq1130@gmail.com>
2025-08-04 10:21:24 +0700

    week3-ha-app/in-app-delete: xoá các file cũ trong cấu trúc thư mục, bao gôm toàn bộ file ha-app, các thư mục con của in-app và in-lib

--- src/app/in-app/in-config/pages/config004-hamper-detail/dataUnitProduct.ts
+++
@@ -1,5 +0,0 @@
-export const DataUnitProduct = [
-    {id: 0, text: 'Tấn'},
-    {id: 1, text:'Kí'},
-    {id: 2, text: 'Gam'}
-]
--- src/app/in-app/in-config/pages/shared/dtos/DTOItemCompany.dto.ts
+++
@@ -1,5 +0,0 @@
-export class DTOItemCompany{
-    id: number;
-    name: string;
-    child: DTOItemCompany[];
-}
