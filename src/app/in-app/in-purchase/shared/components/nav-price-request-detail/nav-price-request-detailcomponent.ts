1daf95e2ddaac5e374e2d073f7a82fbbc196e4e9
hale0904 <leduyha2809@gmail.com>
2025-08-07 21:28:46 +0700

    NavPriceRequestDetail-HTML/SCSS/TS-Add: Tạo component nav cho In-Purchase

---
+++ src/app/in-app/in-purchase/shared/components/nav-price-request-detail/nav-price-request-detailcomponent.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-info-nav',
+  templateUrl: './nav-price-request-detail.component.html',
+  styleUrls: ['./nav-price-request-detail.component.scss']
+})
+export class NavPriceRequestDetailComponent {
+
+}
