c3dfd048b613ab053141962f2a0099c71b9edafe
hale0904 <leduyha2809@gmail.com>
2025-08-07 10:03:46 +0700

    AppModule&InPurchaseModule-TS-Update: Cập nhật khai báo mock API suppliers và orders

--- src/app/in-app/in-purchase/in-purchase.module.ts
+++ src/app/in-app/in-purchase/in-purchase.module.ts
@@ -1,28 +1,36 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import { InputsModule } from '@progress/kendo-angular-inputs';
+import { LabelModule } from '@progress/kendo-angular-label';
+import { DatePickerModule } from "@progress/kendo-angular-dateinputs";
 import { InPurchaseRoutingModule } from './in-purchase-routing.module';
 import { Pur001PriceRequestDetailComponent } from './pages/pur001-price-request-detail/pur001-price-request-detail.component';
 import { Pur001ProductPriceRequestDetailComponent } from './pages/pur001-product-price-request-detail/pur001-product-price-request-detail.component';
-import { HttpClientModule } from '@angular/common/http';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService } from './shared/services/purchase-data.service';
+import { InfoQuoteComponent } from './shared/components/info-quote/info-quote.component';
+import { InputKendoComponent } from './shared/components/input-kendo.component';
+import { DateKendoComponent } from './shared/components/date-kendo.component';
+import { InputAreaKendoComponent } from './shared/components/inputArea-kendo.component';
+import { InfoSuggestComponent } from './shared/components/info-suggest/info-suggest.component';
 
 
 @NgModule({
   declarations: [
     Pur001PriceRequestDetailComponent,
     Pur001ProductPriceRequestDetailComponent,
+    InfoQuoteComponent,
+    InputKendoComponent,
+    DateKendoComponent,
+    InputAreaKendoComponent,
+    InfoSuggestComponent,
   ],
   imports: [
     FormsModule,
     CommonModule,
     InPurchaseRoutingModule,
-    HttpClientModule,
-    HttpClientInMemoryWebApiModule.forRoot(
-      InMemoryDataService, { dataEncapsulation: false }
-    )
+    InputsModule,
+    LabelModule,
+    DatePickerModule,
   ],
   exports: [
     Pur001PriceRequestDetailComponent,
