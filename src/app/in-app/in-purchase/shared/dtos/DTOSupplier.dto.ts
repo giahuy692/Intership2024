49433cb7e720449e2b3870acb3ca7c98fc837778
hale0904 <leduyha2809@gmail.com>
2025-08-07 14:22:47 +0700

    PurchaseDetail-DTO-Update: Cập nhật mới DTO Supplier, sửa lại property date

--- src/app/in-app/in-purchase/shared/dtos/DTOSupplier.dto.ts
+++ src/app/in-app/in-purchase/shared/dtos/DTOSupplier.dto.ts
@@ -3,10 +3,10 @@ import { DTOProductSupplier } from "./DTOProductSupplier.dto";
 export class DTOSupplier {
     code: number = 0;
     name: string = "";
-    date: Date | string;
+    date: Date | string = "";
     state: string = "";
     descrip: string = "";
 
-    dtoProductSupplier: DTOProductSupplier[];
+    dtoProductSupplier: DTOProductSupplier[] = [];
 }
 
