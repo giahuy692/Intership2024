90d5757704d918c74b2f31baa37c59a7638b872b
DESKTOP-SBP7H0D\lehuu <lehuuminhtc30@gmail.com>
2025-08-18 11:52:13 +0700

    khai báo các component mới

--- src/app/p-app/p-purchase/p-purchase-routing.module.ts
+++ src/app/p-app/p-purchase/p-purchase-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PPurchaseComponent } from './p-purchase.component';
+import { Pur010ProposedNewProductListComponent } from './pages/pur010-proposed-new-product-list/pur010-proposed-new-product-list.component';
+import { Config009TaxListComponent } from './pages/config009-tax-list/config009-tax-list.component';
 
 const routes: Routes = [
   {
@@ -10,6 +12,14 @@ const routes: Routes = [
       {
         path: '',
         component: PPurchaseComponent,
+      },
+      {
+        path: "pur010-proposed-new-product-list/:idCompany",
+        component: Pur010ProposedNewProductListComponent,
+      },
+      {
+        path: "config009-tax-list/:idCompany",
+        component: Config009TaxListComponent,
       },
     ]
   },
