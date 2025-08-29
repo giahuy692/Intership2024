e77f8b2ebeb06b8417405746cc215a7b97f4f639
hale0904 <leduyha2809@gmail.com>
2025-08-29 13:49:51 +0700

    p-config-DTO-Add: Thêm DTO mới là DTO PackingUnit

---
+++ src/app/p-app/p-config/shared/dto/DTOPackingUnit.ts
@@ -0,0 +1,8 @@
+export class DTOPackingUnit {
+    Code: number = 0;
+    VNPackingUnit: string = '';
+    JPPackingUnit: string = '';
+    ENPackingUnit: string = '';
+    TypeData: number = 1;
+    OrderBy: number = 0;
+}
